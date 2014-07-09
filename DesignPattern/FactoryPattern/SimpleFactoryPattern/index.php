3659392375d905b2f5bd52cd466d8886c58d39be
SparkLee <liweijsj@163.com>
2014-07-09 10:27:12 +0800

    《士兵&武器》

--- DesignPattern/FactoryPattern/SimpleFactoryPattern/index.php
+++ DesignPattern/FactoryPattern/SimpleFactoryPattern/index.php
@@ -1,1 +1,47 @@
 <?php
+
+/**
+ * 武器接口
+ * @author vriworks5
+ *
+ */
+interface Weapon {
+	static public function getinfo();
+}
+
+/**
+ * AK47自动步枪
+ * @author vriworks5
+ *
+ */
+class AK47 implements Weapon {
+	public function getinfo() {
+      return "我是AK47自动步枪，干爆你，欧耶！";
+	}
+}
+
+/**
+ * AR15轻型气动式步枪
+ * @author vriworks5
+ *
+ */
+class AR15 implements Weapon {
+	public function getinfo() {
+	  return "我是AR15轻型气动式步枪，是不是很屌啊！";
+	}
+}
+
+class Soldier {
+	public $weapon;
+	
+	public function __construct($weapon) {
+		$this->weapon = $weapon;		
+	}
+	
+	public function show_weapon_info() {
+		echo $this->weapon->getinfo();
+	}
+}
+
+$soldier = new Soldier(new AK47());
+$soldier->show_weapon_info();
