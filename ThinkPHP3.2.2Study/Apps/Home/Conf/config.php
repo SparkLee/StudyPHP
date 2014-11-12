cd72f83c8662302b642d6d8bc11c6ed05bd0053f
SparkLee <liweijsj@163.com>
2014-11-12 11:51:39 +0800

    thinkphp路由部分全部学完了

--- ThinkPHP3.2.2Study/Apps/Home/Conf/config.php
+++ ThinkPHP3.2.2Study/Apps/Home/Conf/config.php
@@ -14,6 +14,22 @@ return array(
         //正则路由
         '/^test_router6\/(.*)$/' => 'index/t?name=:1', //对于正则表达式中的每个变量（即正则规则中的子模式）部分，如果需要在后面的路由地址中引用，可以采用:1、:2这样的方式，序号就是子模式的序号。
         
+        //闭包支持：我们可以使用闭包的方式定义一些特殊需求的路由，而不需要执行控制器的操作方法了
+        //-- 规则路由
+        'test_router7/:name' => function ($name) {  
+            echo "My name is:  " . $name;
+        },
+        //-- 正则路由
+        '/^test_router8\/(\d{4})\/(\d{2})$/' => function ($year, $month) { 
+            echo 'year='.$year.'&month='.$month;
+        },
+        //-- 继续执行
+        'test_router9/:name' => function ($name) { //默认的情况下，使用闭包定义路由的话，一旦匹配到路由规则，执行完闭包方法之后，就会中止后续执行。如果希望闭包函数执行后，后续的程序继续执行，可以在闭包函数中使用布尔类型的返回值。
+            echo "wo shi:  " . $name . "<hr>";
+            $_SERVER['PATH_INFO'] = 'index/t';
+            return false; //返回false，继续执行当前模块下index控制器中的t方法。
+        },
+        
         'test_router*/:id' => 'http://baike.baidu.com/subview/1689/:1.htm', //路由地址采用重定向地址的话，如果要引用动态变量，也是采用 :1、:2 的方式。
         'test_router*' => array('http://www.baidu.com', 302),//默认情况下，外部地址的重定向采用301重定向，如果希望采用其它的，可以使用：
         'test_router*' => '/admin', //如果路由地址以“/”或者“http”开头则会认为是一个重定向地址或者外部地址
