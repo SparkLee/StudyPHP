363c7b10dd65886c99dcf20ccbf3da86bebf77ea
SparkLee <liweijsj@163.com>
2014-11-24 10:07:45 +0800

    Revert "Revert "添加两个控制器""
    This reverts commit 2753c5e6b0dc3fa97b427402ce55a4d071823928.

---
+++ ThinkPHP3.2.2Study/Apps/Admin/Controller/SystemController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 系统管理模块
+ * @author vriworks5
+ *
+ */
+namespace Admin\Controller;
+use Think\Controller;
+class SystemController extends Controller {
+    public function index(){
+        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是后台Admin模块的System控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+    }
+}
