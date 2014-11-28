3f8b31e7d47de64d5a32266c701c8eb2e29bffb0
SparkLee <liweijsj@163.com>
2014-11-29 01:58:05 +0800

    添加权限和权限管理

--- ThinkPHP3.2.2Metronic/Application/Admin/Controller/RbacController.class.php
+++ ThinkPHP3.2.2Metronic/Application/Admin/Controller/RbacController.class.php
@@ -30,4 +30,31 @@ class RbacController extends Controller {
         $this->roles = M("role", "think_")->select();
         $this->display();  
     }
+    
+    /**
+     * 添加权限模板展示
+     */
+    public function addNode() {
+        $this->nodes = M("Node")->where("level != 3")->order("level, sort")->select();
+        $this->display();
+    }
+    
+    /**
+     * 添加权限表单处理
+     */
+    public function addNodeHandler(){
+        $node = M("Node");
+        $node->create();
+        if ($node->add()) {
+            $this->success("权限添加成功", U("Rbac/addNode"));
+        }
+    }
+    
+    /**
+     * 权限管理模板展示
+     */
+    public function nodeList() {
+        $this->nodes = M("Node")->order("level, sort")->select();
+        $this->display();
+    }
 }
