de073a95e4062c5f252ba2f28ff3c62b0707010d
SparkLee <liweijsj@163.com>
2015-02-03 16:12:27 +0800

    无

--- PHPExcel1.8.0/index.php
+++ PHPExcel1.8.0/index.php
@@ -32,20 +32,28 @@ function exportExcelDownload($filename, $fileversion) {
         ->setKeywords("我是标记")
         ->setCategory("我是类别");
     
-    //设置默认字体及大小
-    $objPHPExcel->getDefaultStyle()->getFont()->setName('Courier New')->setSize(10);
+    //=================================================================================================================//
+    // 设置单元格字体
+    //=================================================================================================================//
+    $objPHPExcel->getDefaultStyle()->getFont()->setName('Courier New');         //设置当前工作表默认字体
+    $objPHPExcel->getDefaultStyle()->getFont()->setSize(10);                    //设置当前工作表默认字体大小
+    $objPHPExcel->getDefaultStyle()->getFont()->setBold(false);                 //设置当前工作表默认字体加粗
+    $objPHPExcel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);   //设置某单元格字体加粗
+    $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true); //设置某列字体大小自适应
+    
+    //=================================================================================================================//
+    // 设置单元格对齐方式
+    //=================================================================================================================//
+    $objPHPExcel->getDefaultStyle()->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT); //设置当前工作表默认对齐方式
     
     //某个Excel单元格自动换行
     $objPHPExcel->getActiveSheet()->getStyle('A1')->getAlignment()->setWrapText(true);
     
     //设置某行高度
-    $objPHPExcel->getActiveSheet()->getRowDimension(1)->setRowHeight(5);
+    $objPHPExcel->getActiveSheet()->getRowDimension(1)->setRowHeight(50);
     
     //设置某列宽度
     $objPHPExcel->getActiveSheet()->getColumnDimensionByColumn(0)->setWidth(50);
-    
-    //设置某列字体大小自适应
-    $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
     
     //重命名当前工作表名称
     $objPHPExcel->getActiveSheet()->setTitle('我是工作表1');
@@ -53,23 +61,41 @@ function exportExcelDownload($filename, $fileversion) {
     //将第一张工作表设置为当前工作表，以便打开Excel工作薄后默认定位到第一张工作表
     $objPHPExcel->setActiveSheetIndex(0);
     
-    //将内容（区分数据类型）写入Excel单元格
+    //=================================================================================================================//
+    // 设置单元格边框
+    //=================================================================================================================//
+    $objPHPExcel->getActiveSheet()->getStyle('B2')->getBorders()->getAllBorders()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN); //全部边框
+    $objPHPExcel->getActiveSheet()->getStyle('A3')->getBorders()->getTop()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN); //上边框
+    $objPHPExcel->getActiveSheet()->getStyle('A3')->getBorders()->getRight()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN); //右边框
+    $objPHPExcel->getActiveSheet()->getStyle('A3')->getBorders()->getBottom()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN); //下边框
+    $objPHPExcel->getActiveSheet()->getStyle('A3')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN); //左边框
+    $objPHPExcel->getActiveSheet()->getStyle('A3')->getBorders()->getTop()->getColor()->setRGB('cc0000'); //设置边框颜色
+    
+    //=================================================================================================================//
+    // 设置单元格背景色/底纹图案
+    //=================================================================================================================//
+    $objPHPExcel->getActiveSheet()->getStyle('A3')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID); //底纹图案
+    $objPHPExcel->getActiveSheet()->getStyle('A3')->getFill()->getStartColor()->setARGB('FFEEEEEE'); //背景色
+    
+    //=================================================================================================================//
+    // 设置单元格内容
+    //=================================================================================================================//
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', '第一行第一列的内容'); //数据类型：String-Simple
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('B1', '第一行第二列的内容');
     $objPHPExcel->setActiveSheetIndex(0)->setCellValueByColumnAndRow('0', '2', "第二行第一列的内容");
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A3', 'I love my country.');
     
-    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A4', 12); //数据类型：Number-Integer
+    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A4', 10);    //数据类型：Number-Integer（根据填充内容自动判断单元格数据类型  ）
     
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A5', 34.56); //数据类型：Number-Float
     
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A6', -7.89); //数据类型：Number-Negative
     
-    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A7', true); //数据类型：Boolean-True
+    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A7', true);  //数据类型：Boolean-True
     
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A8', false); //数据类型：Boolean-False
     
-    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A9', NULL); //数据类型：NULL
+    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A9', NULL);  //数据类型：NULL
     
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A10', PHPExcel_Shared_Date::PHPToExcel( time() )); //数据类型：Date/Time-Date
     $objPHPExcel->getActiveSheet()->getStyle('A10')->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2);
@@ -88,6 +114,10 @@ function exportExcelDownload($filename, $fileversion) {
     $objPayable->getFont()->setColor( new PHPExcel_Style_Color( PHPExcel_Style_Color::COLOR_DARKGREEN ) );
     $objRichText->createText(', I love u.');
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A13', $objRichText); //数据类型：富文本
+    
+    $objPHPExcel->getActiveSheet()->setCellValue('A14', '=SUM(A4:A5)');      //单元格内容类型：公式 
+    
+    $objPHPExcel->getActiveSheet()->setCellValueExplicit('A15', '847475847857487584', PHPExcel_Cell_DataType::TYPE_STRING); //数据类型：指定单元格数据类型
     
     
     // 将输出重定向到客户端web浏览器
