f394a9db3a82ffd61123e5d323e9367fb254351c
SpkMsysgit <SpkMsysgit@jingzhiheng.com>
2015-05-13 21:54:10 +0800

    测试PHP-MySQL InnoDB存储引擎的事务处理

--- DB/index.php
+++ DB/index.php
@@ -1,31 +1,28 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 try {
-    $pdo = new PDO('mysql:dbname=testdb;host=127.0.0.1', 'root', '');//http://php.net/manual/zh/pdo.construct.php
+    $pdo = new PDO('mysql:dbname=test;host=127.0.0.1', 'root', '');//http://php.net/manual/zh/pdo.construct.php
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
 $pdo->query("SET NAMES UTF8");
 
-/*
-try { //注：因MYSQL的MyISAM引擎不支持事务处理。故，如果表的表类型[引擎]为MyISAM时，事务将失效，同不使用事务一样。
+//MySQL只有InnoDB支持事务处理（如果事务执行无效，请检查事务中涉及到的相应数据表的表类型是否为InnoDB）
+try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->beginTransaction();
     
-    $pdo->exec("INSERT INTO student(name, age) VALUE('中国人', 26)");
-    $pdo->exec("INSERT INTO student(name, age) VALUE('中国人', 26)");
-    throw new Exception();
-    $pdo->exec("INSERT INTO student(name, age) VALUE('中国人', 26)");
+    $pdo->exec("INSERT INTO user(name, age, gender) VALUE('LiWei', 26, 1);");
+    $pdo->exec("INSERT INTO user(name, age, gender) VALUE('LiWei', 26, 1);");
+    $pdo->exec("INSERT INTO user(name, age, gender) VALUE('LiWei', 26, 1);");
+    $pdo->exec("INSERT INTO user(name, age, gender) VALUE('LiWei', 26, 1);");
+    $pdo->exec("INSERT INTO user(name, age, gender) VALUE('LiWei', 26, 1);");
+    $pdo->exec("INSERT INTO user(name, age, gender) VALUE('LiWei', 26, 1);");
+    throw new Exception("定制的异常。");
+    $pdo->exec("INSERT INTO user(name, age, gender) VALUE('ChineseMan', 26, 1);");
     
     $pdo->commit();
 } catch (Exception $e) {
     $pdo->rollBack();
     echo "数据库操作失败: " . $e->getMessage();
-}*/
-
-$i = 10000000;
-while ($i > 0) {
-    $i--;
-    $pdo->exec("INSERT INTO student(name, age) VALUE('中国人', 26)");
-    echo $i;
 }
