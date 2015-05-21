444c2f720dc755a21642b181909a5140448e29ae
sparklee2013 <liweijsj@163.com>
2015-05-21 10:28:42 +0800

    Laravel学习-初识 Eloquent，创建 Comment 模型，建立“一对多关系”
    Laravel学习-初识 Eloquent，创建 Comment 模型，建立“一对多关系”，前台评论提交功能，后台评论管理功能

--- Laravel/app/Http/routes.php
+++ Laravel/app/Http/routes.php
@@ -29,8 +29,11 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 Route::get('pages/{id}', 'PagesController@show');
 
+Route::post('comment/store', 'CommentsController@store');
+
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function()
 {
   Route::get('/', 'AdminHomeController@index');
   Route::resource('pages', 'PagesController');
+  Route::resource('comments', 'CommentsController');
 });
--- Laravel/app/Page.php
+++ Laravel/app/Page.php
@@ -1,9 +1,12 @@
-<?php namespace App;
+<?php
+
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model {
 
-	//
-
+  public function hasManyComments() {
+    return $this->hasMany('App\Comment', 'page_id', 'id');
+  }
 }
